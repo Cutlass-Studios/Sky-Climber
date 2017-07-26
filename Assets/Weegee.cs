18ed9fe6c439f575a7e8acca2ff450583dec9bcd
Chris Marcok <chris.marcok@gmail.com>
2017-07-26 01:23:48 -0400

    added comments
    coments

--- Assets/Weegee.cs
+++ Assets/Weegee.cs
@@ -6,69 +6,94 @@ using UnityEngine.UI;
 
 public class Weegee : MonoBehaviour
 {
-
-
-
+    //--- WEEGEE PROPERTIES ---
+    //Movement
     public bool facingRight = true;
     public int playerSpeed = 10;
     public int playerJumpPower = 30;
     private bool isMovingLeft = false;
     private bool isMovingRight = false;
-    public int jumpCount = 0;
+    //Weegee's current and previous Y Coordinates
     private float lastY = 0.0f;
     private float thisY = 0.0f;
-    public GameObject cam;
-    public int level = 0;
-    public int blockNum = 1;
-    //checks to make sure level doesnt happen twice 
-    public int levelCheck = 0;
-    //lastX is position of previous block so next block is within jumping distance
-    float lastX = 0;
-    //Stores text for score and high score
-    public Text countText, highScore, totalCoins, deathScore, deathHigh;
-    int high = 0;
+    //Used to limit the amount of times Weegee can Jump
+    public int jumpCount = 0;
+    //Animations
     public Sprite jump;
     public Sprite idle;
-    public AudioSource audio1;
+
+    //--- WORLD GAME OBJECTS ---
+    public GameObject cam;
+    //Stores text for score and high score which will appear on the User Interface
+    public Text countText, highScore, totalCoins, deathScore, deathHigh, timerText;
+    //Collectables
+    public GameObject bigJump;
+    public GameObject coin;
+    //Menus
+    public GameObject gameButtons;
+    public GameObject mainMenu;
+    public GameObject deathMenu;
+    //Sounds
+    //Responsible for fucking nothing, kys chris
+    public AudioSource audio1; //NOTE: Redundant, audio1 is only used to play the jump sound, which weegee makes, just merge with audio2
+                               //Responsible for Handling collectable sounds (coins, powerups) and sounds Weegee Makes
     public AudioSource audio2;
     public AudioClip die;
     public AudioClip jumpSound;
     public AudioClip yahoo;
-    public Sprite[] blockTypes;
     public AudioClip coinSound;
-    public GameObject coin;
-    public bool falling;
-    public int coins;
-    public GameObject gameButtons;
-    public GameObject mainMenu;
-    public GameObject deathMenu;
+    //Textures
+    public Sprite[] blockTypes;
     public Sprite muted, soundOn;
-    public bool sound;
-    public GameObject bigJump;
-    public float startTime;
-    public Text timerText;
 
+    //--- GAME LOGIC ---
+    public int level = 0;
+    public int blockNum = 1;
+    public bool falling;
+    //Used to make sure Weegee doesn't drop down to a previous level and go back up to a new level in attempt to increase the level
+    public int levelCheck = 0;
+    //lastX is position of previous block so next block is within jumping distance
+    float lastX = 0;
+    //Is Sound On
+    public bool sound;
+    //Timer - A float which indicates the beginning of an event (used for Powerups)
+    public float startTime;
+    //used to display the highscore and number of coins found in the PlayerPrefs (these values are stored in memory)
+    public int high = 0;
+    public int coins;
+
+
+
+    //Method that is called when game begins
     private void Start()
     {
+        //Force the Screen into landscape
         Screen.orientation = ScreenOrientation.Landscape;
+
+        //Displays only main menu upon start
         gameButtons.SetActive(false);
         deathMenu.SetActive(false);
         mainMenu.SetActive(true);
         timerText.gameObject.SetActive(false);
+
+        //Retrieve highscore and coins from memory, and then display them
         high = PlayerPrefs.GetInt("highscore", high);
         coins = PlayerPrefs.GetInt("coins", coins);
         highScore.text = "Highscore: " + high;
         totalCoins.text = "Coins: " + coins;
+
+        //Set the Score (will be zero because the game just started)
         setScore();
-        falling = false;
+        falling = false; //NOTE: be set when instantiated
     }
 
     public void toggleSound()
     {
+        //if sound is on, turn it off and vice versa
         if (sound)
         {
             mainMenu.transform.GetChild(0).gameObject.GetComponent<Image>().sprite = muted;
-            
+
         }
         else
         {
@@ -79,60 +104,82 @@ public class Weegee : MonoBehaviour
         audio2.mute = !audio2.mute;
     }
 
-    // Update is called once per frame
+    // Update is called once per frame (60fps)
     public void Update()
     {
-
+        //If the time since start exceeds 10 seconds after a powerup is taken (10s limit), AND the BigJumps Powerup is active, remove timer and limit playerJumpPower
         if (startTime + 10 <= Time.realtimeSinceStartup && playerJumpPower > 30)
         {
             timerText.gameObject.SetActive(false);
             playerJumpPower = 30;
         }
-        if (playerJumpPower > 30) timerText.text = ((int) (startTime + 10 - Time.realtimeSinceStartup)).ToString();
 
+        //If a player has a bigJumps Powerup, enable a timer which countsdown from 10, only displays int
+        if (playerJumpPower > 30)
+        {
+            timerText.text = ((int)(startTime + 10 - Time.realtimeSinceStartup)).ToString();
+        }
+
+        //Change sprites depending if Weegee is mid jump
         if (jumpCount > 0)
+        {
             gameObject.GetComponent<SpriteRenderer>().sprite = jump;
+        }
         else
+        {
             gameObject.GetComponent<SpriteRenderer>().sprite = idle;
+        }
 
-
+        //lastLevel becomes what level is. level updates
         int lastLevel = level;
         level = (int)Math.Floor(gameObject.GetComponent<Rigidbody2D>().position.y) / 4 + 2;
 
-        //checks if it goes up a level. doesjnt check going down then going back up
+        //checks if Weegee goes up a level. doesn't check going down then going back up
         if (level > lastLevel && level > levelCheck)
         {
             levelCheck = level;
 
+            //check if gameButtons setActive == true (isShowing). if the player is playing the game, spawn the next block.
             if (gameButtons.activeInHierarchy)
             {
-                Debug.Log("ACTIVE");
+                //Debug.Log("ACTIVE");
                 spawnBlock(0);
             }
 
             setScore();
             moveCloud();
         }
+
+        //Always check if the player is exceeding 2 jumps
         DoubleJumpCheck();
+        //Camera moves Weegee's Y position
         CameraMove();
+        //used to spawn the coins during the falling phase
         spawnCoin();
-        //Used for keyboard testing (can be removed)
+
+        //Used for keyboard testing (can be removed during android release)
         if (Input.GetButtonDown("JumpKeyboard")) Jump();
         if (Input.GetButtonDown("LeftKeyboard")) MoveLeft();
         if (Input.GetButtonDown("RightKeyboard")) MoveRight();
+        //if any button is released, stop weegee
         if (Input.GetButtonUp("LeftKeyboard") || Input.GetButtonUp("RightKeyboard")) Stop();
-        //Movement is the only thing requiring updates
+
+        //moves the player
         MovePlayer();
-        //resets when player dies
+        //resets level and game when player dies
         Reset();
 
     }
 
+    //When the play button in the Main Menu is pressed
     public void Play()
     {
+        //Deactivated all unneccesary buttons, activate play buttons
         gameButtons.SetActive(true);
         deathMenu.SetActive(false);
         mainMenu.SetActive(false);
+
+        //spawn the first 3 blocks because we are incapable of doing this properly. higher number = lower in y position.
         spawnBlock(8);
         spawnBlock(4);
         spawnBlock(0);
@@ -162,11 +209,13 @@ public class Weegee : MonoBehaviour
 
     }
 
+    //A BigJump powerup is instantiated at given coordinates.
     public void spawnBigJump(float x, float y)
     {
-        Instantiate(bigJump, new Vector3(x, y, 0) , Quaternion.identity);
+        Instantiate(bigJump, new Vector3(x, y, 0), Quaternion.identity);
     }
 
+    //constantly check if player is giving input, if so, move the player.
     public void MovePlayer()
     {
         if (isMovingRight)
@@ -179,29 +228,39 @@ public class Weegee : MonoBehaviour
         }
     }
 
+    //constantly checks (from update) if weegee is able to jump.
     public void DoubleJumpCheck()
     {
         lastY = thisY;
         thisY = gameObject.GetComponent<Rigidbody2D>().velocity.y;
+
+        //If weegee has no y velocity for two frames (last and current frame), reset the jumpcount, so that he may jump again
         if (lastY == 0.0f && thisY == 0.0f)
         {
             jumpCount = 0;
         }
+
+        //If weegee walks off a platform, count it as a first jump (weegee cant walk off a platform and jump twice midair).
         if (thisY < 0 && jumpCount == 0)
         {
             jumpCount = 1;
         }
     }
 
+    //Camera follows Weegee in the Y Axis.
     public void CameraMove()
     {
+        //Camera becomes the object in the Unity's hierarchy
         cam = GameObject.Find("Main Camera");
-        //Camera doesn't move down if Weegee is below
+
+        //Camera doesn't move down if Weegee is below it's Y coordinate. unused, acts like mario's camera in NES super mario (never moves left)
         //if(gameObject.GetComponent<Rigidbody2D>().position.y > cam.GetComponent<Transform>().position.y)
+
         cam.GetComponent<Transform>().position = new Vector3(cam.GetComponent<Transform>().position.x, gameObject.GetComponent<Rigidbody2D>().position.y, cam.GetComponent<Transform>().position.z);
 
     }
 
+    //Moves weegee left, flips his sprite given that he is facing right
     public void MoveLeft()
     {
         isMovingLeft = true;
@@ -212,12 +271,7 @@ public class Weegee : MonoBehaviour
 
     }
 
-    public void Stop()
-    {
-        isMovingLeft = false;
-        isMovingRight = false;
-    }
-
+    //similar to moveLeft
     public void MoveRight()
     {
         isMovingRight = true;
@@ -227,58 +281,84 @@ public class Weegee : MonoBehaviour
         }
     }
 
+    //Called when direcetional key is released, to stop weegee's position. assumes both directions are never pressed together.
+    public void Stop()
+    {
+        isMovingLeft = false;
+        isMovingRight = false;
+    }
+
+    //Spawns a block at a specified Y position (NOT coordinate) RELATIVE TO WEEGEE, not the world
     public void spawnBlock(int y)
     {
-        //  if (Input.GetKeyDown(KeyCode.P))
-        //  {
+        //oldBlock points to a dirt texture found within unity
         GameObject oldBlock = GameObject.Find("mc_dirt (14)");
+        //Mainblock is used when new blocks are instantiated. Mainblock becomes the parent for the newly instantiated blocks.
         GameObject mainBlock = GameObject.Find("mc_dirt");
+
         System.Random random = new System.Random();
-
         float randX;
-
 
         do
         {
+            //randX, used for the X position of the next block placement, can't be too far away from the previous block (prevent impossible jumps)
             randX = random.Next((int)(lastX - 7), (int)(lastX + 7));
 
         } while (randX > 8 || randX < -8);
 
-
+        //create a  random Y number, so that not all blocks are the same distance apart in the Y
         float randY = random.Next(-1, 0);
 
+        //The position for the new block, not actually the vector for the old block
         Vector3 oldBlockVector = new Vector3(randX, level * 4 + randY - y, 0);
+        //instantiate another block as a new gameobject (Object original, Vector3 position, Quaternion rotation, Transform parent)
         GameObject block = (GameObject)Instantiate(oldBlock, oldBlockVector, Quaternion.identity, mainBlock.transform);
+        //the blocks new name increases so that no 2 blocks have the same name
         block.name = "Block #" + blockNum;
+
+        //every 10 blocks, spawn a bigJump powerup 2 above the block's y
         if (blockNum % 10 == 0)
         {
             spawnBigJump(block.GetComponent<Transform>().position.x, block.GetComponent<Transform>().position.y + 2);
         }
+
+        //Set the sprites of the blocks, changes every 20 score. after 180, will always be the same texture.
         if (levelCheck - 2 > 180)
         {
             block.GetComponent<SpriteRenderer>().sprite = blockTypes[8];
         }
         else
             block.GetComponent<SpriteRenderer>().sprite = blockTypes[(levelCheck) / 20];
+
+        //each time a block is instantiated, increase the number for the next block.
         blockNum++;
+
+        //Destroys the block that was instantiated 4 blocks ago. This means only 4 blocks will ever be instantiated at any time.
         if (level >= 4)
             Destroy(GameObject.Find("Block #" + (level - 3)));
+
+        //lastX becomes the X that was just used, so that the next X can be with respect to this one's.
         lastX = randX;
 
     }
 
+    //called when space/jump is pressed
     public void Jump()
     {
         if (jumpCount < 2)
         {
+            //play sound
             audio1.clip = jumpSound;
             audio1.Play();
+
+            //sets weegee's Y velocity to something new (in this case, the playerJumpPower). also increase the number of jump's weegee has done (resets when he touches ground)
             GetComponent<Rigidbody2D>().velocity = new Vector2(gameObject.GetComponent<Rigidbody2D>().velocity.x, playerJumpPower);
             jumpCount++;
         }
 
     }
 
+    //changes view of sprite. called by movement.
     void FlipPlayer()
     {
         facingRight = !facingRight;
@@ -286,36 +366,47 @@ public class Weegee : MonoBehaviour
         localScale.x *= -1;
         transform.localScale = localScale;
     }
-    //reset
+
+    //reset, always called by update()
     public void Reset()
     {
+        //only execute if above level 2, AND weegee is on the ground (aka below y coord of -2)
         if (levelCheck > 2 && gameObject.GetComponent<Rigidbody2D>().position.y <= -2.0f)
         {
             deathScore.text = "Score: " + (levelCheck - 2);
 
+            //play sound
             audio2.clip = die;
             audio2.Play();
+
+            //destroy all remaining blocks
             levelCheck = blockNum;
             while (levelCheck > 0)
             {
                 Destroy(GameObject.Find("Block #" + (levelCheck)));
                 levelCheck--;
             }
-            levelCheck = 0;
 
+            //change all variables so that game can be played from the start again
+            levelCheck = 0;
             level = 0;
             blockNum = 1;
+            //player now on ground. 
             falling = false;
 
+            //create an area for every game object with certain tag. destroys all these items. in this case destroys all coins (so player doesnt see them when going back up)
             GameObject[] objs = GameObject.FindGameObjectsWithTag("Coin");
             foreach (GameObject target in objs)
             {
                 GameObject.Destroy(target);
             }
+
+            //enables menus and makes sure that all powerups are set off. 
             deathMenu.SetActive(true);
             gameButtons.SetActive(false);
             deathHigh.text = "Highscore: " + (high);
             playerJumpPower = 30;
+            timerText.gameObject.SetActive(false);
         }
 
     }
@@ -324,25 +415,33 @@ public class Weegee : MonoBehaviour
     //sets text for score
     public void setScore()
     {
-
+        //score is actually the player's highest level - 2. displays the score by changing the text
         int tempScore = levelCheck - 2;
         countText.text = "Score: " + (tempScore).ToString();
+
+        //store the new value of player's coins
         PlayerPrefs.SetInt("coins", coins);
+
+        //if a player beats their highscore, set new highscore and store the data in playerprefs
         if (tempScore > high)
         {
             highScore.text = "Highscore: " + (tempScore);
             high = tempScore;
             PlayerPrefs.SetInt("highscore", high);
         }
+
+        //play a sound every 10 levels, aslong as you aren't on level 0
         if ((tempScore) % 10 == 0 && (tempScore) != 0)
         {
             audio2.clip = yahoo;
             audio2.Play();
         }
     }
+
     //moves cloud above luigi for """"realism""""
     public void moveCloud()
     {
+        //Every number of blocks, assuming weegee isnt on the ground, move the cloud to a random pos
         if ((blockNum - 2) % 4 == 0 && (blockNum - 2) != 0)
         {
             System.Random random = new System.Random();
@@ -351,6 +450,7 @@ public class Weegee : MonoBehaviour
         }
     }
 
+    //Called when returning to the main menu, toggles all other buttons off.
     public void goToMenu()
     {
         gameButtons.SetActive(false);
@@ -358,9 +458,11 @@ public class Weegee : MonoBehaviour
         mainMenu.SetActive(true);
     }
 
+    //Whenever Weegee enters the trigger for another object, this method is called.
     void OnTriggerEnter2D(Collider2D collision)
     {
-
+        //If the object weegee collides with has a certain tag (similar to an assigned property, given in unity), execute the if statement
+        //Collectables are destroyed upon being touched. 
         if (collision.gameObject.CompareTag("bigJumps"))
         {
             timerText.gameObject.SetActive(true);
