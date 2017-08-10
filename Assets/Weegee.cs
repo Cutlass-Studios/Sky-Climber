d400dc35a3387b767a5372410e1e389373e75793
Christopher Goel <christopher.goel@gmail.com>
2017-08-10 19:48:38 -0400

    Minor updates
    - Sky Climber is now white with black outline
    - Game now auto rotates when phone rotates

--- Assets/Weegee.cs
+++ Assets/Weegee.cs
@@ -176,6 +176,14 @@ public class Weegee : MonoBehaviour
     // Update is called once per frame (60fps)
     public void Update()
     {
+        if (Input.deviceOrientation == DeviceOrientation.LandscapeRight)
+        {
+            Screen.orientation = ScreenOrientation.LandscapeRight;
+        }
+        else if (Input.deviceOrientation == DeviceOrientation.LandscapeLeft)
+        {
+            Screen.orientation = ScreenOrientation.LandscapeLeft;
+        }
 
         if (jumpCount > 0)
         {
